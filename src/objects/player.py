30a9ed0624c7dc9d5b5641d7824075ac3b1de2c9
David Ward <dward@semquest.com>
2025-02-20 13:22:18 -0800

    moving with mouse, test with keyboard

--- src/objects/player.py
+++ src/objects/player.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import numpy.typing as npt
 import src.engine.settings as settings
 from src.objects.txt_item import Txt_item
 
@@ -14,10 +15,16 @@ class Player:
         self.graphic: pygame.Surface = self.inp_graphic.copy()
         self.mode: str = "waiting"
         self.angle: float = 0
-        self.cx, self.cy = self.graphic.get_size()
+        self.cpos: pygame.Vector2 = pygame.Vector2(self.graphic.size) / 2
 
-        self.grid = np.array([[0, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 0]])
-        self.gx, self.gy = 0, 0
+        NDArrayInt = npt.NDArray[np.int_]
+        self.grid: NDArrayInt = np.array(
+            [[0, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 0]]
+        )
+        self.gx: int = 0
+        self.gy: int = 0
+        self.move_delta: tuple[int, int] = (0, 0)
+        self.mouse_pos: pygame.Vector2 = pygame.Vector2(0, 1)
 
     def new_pos_xy(self, x, y):
         self.pos = pygame.Vector2(x, y)
@@ -28,31 +35,74 @@ class Player:
     def process_event(self, event):
         if self.mode == "waiting":
             if event.type == pygame.MOUSEBUTTONDOWN:
+                self.mouse_pos = event.pos
                 self.mode = "click_start"
                 print("click down")
         elif self.mode == "button_down":
             if event.type == pygame.MOUSEBUTTONUP:
                 self.mode = "click_end"
                 print("click up")
+            elif event.type == pygame.MOUSEMOTION:
+                self.mouse_pos = event.pos
+
+        # temporary code to test stuff
+        if event.type == pygame.KEYDOWN:
+            if event.key in [pygame.K_UP, pygame.K_w]:
+                self.move_delta = (0, -1)
+            elif event.key in [pygame.K_DOWN, pygame.K_s]:
+                self.move_delta = (0, 1)
+            elif event.key in [pygame.K_LEFT, pygame.K_a]:
+                self.move_delta = (-1, 0)
+            elif event.key in [pygame.K_RIGHT, pygame.K_d]:
+                self.move_delta = (1, 0)
 
     def update(self, dt):
         if self.mode == "click_start":
             print("in click start")
             self.mode = "button_down"
         elif self.mode == "button_down":
-            self.angle += 1
+            # x,y reversed to accommodate angle_to function
+            v2m = pygame.Vector2(self.mouse_pos[1], self.mouse_pos[0]) - pygame.Vector2(
+                self.pos[1], self.pos[0]
+            )
+            self.angle = 360.0 - v2m.angle_to(pygame.Vector2(-1, 0))
             self.graphic = pygame.transform.rotate(self.inp_graphic, self.angle)
-            self.cx, self.cy = self.graphic.get_size()
-            self.cx = self.cx / 2
-            self.cy = self.cy / 2
+            self.cpos = pygame.Vector2(self.graphic.size) / 2
+
+            vmove_delta = (self.mouse_pos - self.pos).normalize()
+            self.pos += vmove_delta
+
         elif self.mode == "click_end":
             self.mode = "waiting"
 
-    def draw(self, window):
-        window.blit(
-            self.graphic, (self.pos[0] - self.cx, self.pos[1] - self.cy, 128, 128)
+        # Prevent exit screen
+        trygx = max(self.gx + self.move_delta[0], 0)
+        trygx = min(
+            trygx,
+            int(settings.screensize[0]) // settings.blocksize - self.grid.shape[0],
+        )
+        trygy = max(self.gy + self.move_delta[1], 0)
+        trygy = min(
+            trygy,
+            int(settings.screensize[1]) // settings.blocksize - self.grid.shape[1],
         )
 
+        # prevent collision with grid
+        food = settings.STATES["GamePlay"].food
+
+        grid_portion = food[
+            trygy : trygy + self.grid.shape[1],
+            trygx : trygx + self.grid.shape[0],
+        ]
+        collision = np.any(grid_portion * self.grid)
+
+        if not collision:
+            self.gx = trygx
+            self.gy = trygy
+
+        self.move_delta = (0, 0)
+
+    def draw(self, window):
         for y in range(self.grid.shape[0]):
             for x in range(self.grid.shape[1]):
                 if self.grid[y, x]:
@@ -63,3 +113,6 @@ class Player:
                         settings.blocksize,
                     )
                     pygame.draw.rect(window, pygame.Color("grey20"), r)
+
+        drawxy = self.pos - self.cpos
+        window.blit(self.graphic, (*drawxy, *self.graphic.size))
--- src/screens/gameplay.py
+++ src/screens/gameplay.py
@@ -13,7 +13,8 @@ class GamePlay:
         self.done: bool = False
         self.next_state: str = "GamePlay"
 
-        self.player: Player = Player(0.5 * settings.screensize)
+        startxy = settings.edge * settings.blocksize // 2
+        self.player: Player = Player(pygame.Vector2(startxy, startxy))
 
         self.score: int
         self.score_text: Txt_item = Txt_item("Score:", (100, 30))
@@ -30,11 +31,6 @@ class GamePlay:
         dx = int(settings.screensize[0]) // bs - edge * 2
         dy = int(settings.screensize[1]) // bs - edge * 2
         self.food = get_blocks(dx, dy, edge)
-        for y in range(self.food.shape[0]):
-            for x in range(self.food.shape[1]):
-                if self.food[y, x]:
-                    r = pygame.Rect(x * bs, y * bs, bs - 1, bs - 1)
-                    pygame.draw.rect(self.background, pygame.Color("white"), r)
 
     def startup(self):
         pass
@@ -51,6 +47,14 @@ class GamePlay:
 
     def draw(self, window):
         window.blit(self.background, (0, 0))
+
+        bs = settings.blocksize
+        for y in range(self.food.shape[0]):
+            for x in range(self.food.shape[1]):
+                if self.food[y, x]:
+                    r = pygame.Rect(x * bs, y * bs, bs - 1, bs - 1)
+                    pygame.draw.rect(self.background, pygame.Color("white"), r)
+
         self.score_text.render(window)
 
         self.player.draw(window)
